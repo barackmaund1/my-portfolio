d5b34c1372ae8064cb1af615d9382cdf2e487c7b
barackmaund1 <pmaundu49@gmail.co>
2020-07-02 21:37:33 +0300

    alter the models

--- static/js/newsletter.js
+++ static/js/newsletter.js
@@ -9,12 +9,16 @@ $(document).ready(function(){
         'data':form.serialize(),
         'dataType':'json',
         'success': function(data){
-          alert(data['success'])
-        },
+          Swal.fire(
+            '',
+            data['success'],
+            'success'
+          )
+        }
       })// END of Ajax method
       $('#id_name').val('')
       $("#id_email").val('')
-      $("#id_subject").val('')
+      $("#id_contact").val('')
       $("#id_message").val('')
     }) // End of submit event
   
