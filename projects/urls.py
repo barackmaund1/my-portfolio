82260516969e5e1e3b54ce0e39ea696ff600debe
barackmaund1 <pmaundu49@gmail.co>
2020-07-05 00:37:46 +0300

    deploy

--- projects/urls.py
+++ projects/urls.py
@@ -4,7 +4,7 @@ from .views import PostListView
 urlpatterns = [
     
     path('', PostListView.as_view(), name='projects'),
-    path('post', views.createpost, name='create-post'),
+    path('post/', views.createpost, name='create-post'),
 
     path('ajax/newsletter/', views.newsletter, name='newsletter')
 
